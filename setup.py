8b092e091f10f030439c502c2260785d0c7531cf
Aman Bhargava <amanb2000@gmail.com>
2024-04-10 00:46:11 -0700

    Add setup.py for languagegame package import

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='languagegame',
+    version='0.1.0', 
+    description='Horizontally scaling LLM capabilities via Language Games.',
+    packages=['languagegame']
+)
